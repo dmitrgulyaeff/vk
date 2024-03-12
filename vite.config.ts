13d0601e93d8aedca2c051bb1302815866a52b38
dmitrgulyaeff <dmitrgulyaeff@yandex.ru>
2024-03-13 02:21:43 +0700

    feat: добавил Dockerfile и скрипт для поднятия контейнера

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import { babel } from '@rollup/plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [babel({ extensions: ['.ts', '.tsx'], babelHelpers: 'bundled' }), react()],
+  plugins: [
+    babel({ extensions: ['.ts', '.tsx'], babelHelpers: 'bundled' }),
+    react(),
+  ],
+  server: {
+    port: 3030,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 5675,
+  },
 });
